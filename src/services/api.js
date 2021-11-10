1ae909440fcf53da8c733a4bc8dd3feecb5e6e8c
Carlos L. Santos <carlos@openshift.com.br>
2021-11-10 20:51:02 -0300

    console.log em api.getHost()

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 import { getHost } from '../utils/utils-context';
 
+const host = getHost();
+console.log('api.getHost:', host);
+
 const api = axios.create({
     //baseURL: 'http://localhost:8000',
-    //baseURL: getHost(),
-    baseURL: 'https://api.mde.com.br',
+    baseURL: host,
+    //baseURL: 'https://api.mde.com.br',
     withCredentials: false,
     headers: { 
       'Access-Control-Allow-Origin' : '*',
