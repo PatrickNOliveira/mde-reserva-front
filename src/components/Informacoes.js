fac0fe8ae0cfd438b39db13cfd75d846b5097668
Carlos L. Santos <carlos@openshift.com.br>
2021-12-10 21:44:59 -0300

    build 1.0.27

--- src/components/Informacoes.js
+++ src/components/Informacoes.js
@@ -9,7 +9,7 @@ import api from '../services/api';
 
 export default function Informacoes() {
 
-    const build   = '1.0.26';
+    const build   = '1.0.27';
     
     const { id }  = useParams();
     const history = useHistory();
