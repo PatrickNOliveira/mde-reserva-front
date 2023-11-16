f155fb62ccfeb8514c258d49145012729c7f3145
Patrick Nascimento <patrickndeoliveira@gmail.com>
2023-11-16 17:02:55 -0300

    Deploy

--- src/utils/utils-context.js
+++ src/utils/utils-context.js
@@ -110,8 +110,8 @@ export function contaEncerrada(login) {
 }
 
 export function getHost() {
-   return 'http://localhost:8000'; // desenvolvimento
-   //return 'https://api.mde.com.br:8080'; // produção
+   //return 'http://localhost:8000'; // desenvolvimento
+   return 'https://api.mde.com.br:8080'; // produção
    //return 'http://api2.mde.com.br:9091'; 
 }
 
