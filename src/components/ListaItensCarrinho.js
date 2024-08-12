734e9cb365fb342d5984bab78111d067dd4af86a
Patrick Nascimento <patrickndeoliveira@gmail.com>
2024-08-12 19:50:41 -0300

    Ajuste no cancelamento do carrinho

--- src/components/ListaItensCarrinho.js
+++ src/components/ListaItensCarrinho.js
@@ -1,38 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 //import '../styles/Requests.css';
 import getPreco from '../utils/getPreco'
 import alerta from '../utils/alertas';
 import api from '../services/api';
 import styled from 'styled-components';
-import { useHistory } from "react-router-dom";
+import {useHistory} from "react-router-dom";
 
-import { 
+import {
     getLogin, getMesaAtual, setLogin,
     setMesaAtual, getConta, contaEncerrada,
-    getCardapioAtual, getSuiteAtual, setApartamentoAtual, getApartamentoAtual 
+    getCardapioAtual, getSuiteAtual, setApartamentoAtual, getApartamentoAtual
 } from '../utils/utils-context';
 
 import LocalEntrega from './LocalEntrega';
 
 export default function ListaItensCarrinho({suite_id, onCarrinhoVazio, onFinalizaPedido}) {
-    
+
     const history = useHistory();
-    const [carrinho, setCarrinho] = useState({ items:[], total: 0});
-    const [mensagem, setMensagem ] = useState('Aguarde...');
+    const [carrinho, setCarrinho] = useState({items: [], total: 0});
+    const [mensagem, setMensagem] = useState('Aguarde...');
     const [login] = useState(getLogin());
     const [mesa] = useState(
         getLogin().suite_id ? null : getMesaAtual()
     );
-    
-    const [ suite, setSuite ] = useState(getSuiteAtual());
+
+    const [suite, setSuite] = useState(getSuiteAtual());
 
     const [locais] = useState(getConta()['locais_entrega']);
     const [cardapio] = useState(getCardapioAtual());
-    
+
     const [localizacao, setLocalizacao] = useState(
         getConta()['locais_entrega'] ? getConta()['locais_entrega'][0] : ''
     );
-    
+
     const [conta] = useState(getConta());
 
     const [mensagemLocalizacao, setMensagemLocalizacao] = useState(conta.obsLocal);
@@ -42,7 +42,7 @@ export default function ListaItensCarrinho({suite_id, onCarrinhoVazio, onFinaliz
     const isCheckout = () => {
         if (contaEncerrada(login)) {
             setLogin(null);
-            history.push({ pathname: `/${login.uuid}` });
+            history.push({pathname: `/${login.uuid}`});
             return true;
         }
     }
@@ -57,9 +57,9 @@ export default function ListaItensCarrinho({suite_id, onCarrinhoVazio, onFinaliz
             setMensagem('Sem ítens!');
             return;
         }
-        
-       // let url = '';
-        
+
+        // let url = '';
+
         const apartamento = getApartamentoAtual();
 
         const ap = login.suite ?? apartamento.id;
@@ -90,10 +90,10 @@ export default function ListaItensCarrinho({suite_id, onCarrinhoVazio, onFinaliz
             mounted = false;
         }
 
-    },[mesa, suite_id]);
+    }, [mesa, suite_id]);
 
     const onItemClick = (item) => {
-        
+
         if (isCheckout()) return;
 
         //let url = '';
@@ -109,16 +109,23 @@ export default function ListaItensCarrinho({suite_id, onCarrinhoVazio, onFinaliz
 
         //let url = '/api/order/cart/' + (suite_id ? '' : ('waiter/' + login.uuid + '/')) + (suite_id ?? mesa);
 
-        alerta({ 
+        alerta({
             title: 'Atenção!',
             message: 'Excluir esse ítem?',
-            onYes: () => { 
+            onYes: () => {
                 api.delete(url).then(response => {
                     if (response.data.total === 0)
-                        setMesaAtual(null);    
+                        setMesaAtual(null);
                     setCarrinho(response.data);
-                });                                
-             }
+                }).catch(e => {
+                    const index = carrinho.items.indexOf(carrinho.items.find(i => i.id === item.id))
+                    const novoCarrinho = index !== -1 ? carrinho.items.slice(0, index).concat(carrinho.items.slice(index + 1)) : carrinho.items
+                    setCarrinho(prev => ({
+                        total: prev.total - item.preco,
+                        items: novoCarrinho
+                    }))
+                });
+            }
         })
 
     }
@@ -131,7 +138,7 @@ export default function ListaItensCarrinho({suite_id, onCarrinhoVazio, onFinaliz
         }
 
         if (localizacaoObrigatoria(localizacao) && observacao.trim() === '') {
-            alerta({ 
+            alerta({
                 title: 'Atenção!',
                 message: 'Informe detalhes de sua localização!'
             });
@@ -141,16 +148,16 @@ export default function ListaItensCarrinho({suite_id, onCarrinhoVazio, onFinaliz
     }
 
     const onEnviarPedidoClick = () => {
-        
+
         if (isCheckout() || !isValid()) return;
 
         const apartamento = getApartamentoAtual();
         const ap = login.suite ?? apartamento.id;
 
-        alerta({ 
+        alerta({
             title: 'Atenção!',
             message: 'Enviar esse pedido?',
-            onYes: () => { 
+            onYes: () => {
                 api.post('/api/order/cart', {
                     suite_id: ap, //suite ?? suite_id,
                     mesa: mesa,
@@ -173,7 +180,7 @@ export default function ListaItensCarrinho({suite_id, onCarrinhoVazio, onFinaliz
                     onFinalizaPedido();
                 });
             }
-        });        
+        });
 
     }
 
@@ -184,7 +191,7 @@ export default function ListaItensCarrinho({suite_id, onCarrinhoVazio, onFinaliz
         })[0];
 
         setLocalizacao(localizacaoAtual);
-        
+
         const texto = localizacaoAtual.texto ? localizacaoAtual.texto : conta.obsLocal ? conta.obsLocal : '';
 
         setMensagemLocalizacao(texto);
@@ -195,15 +202,15 @@ export default function ListaItensCarrinho({suite_id, onCarrinhoVazio, onFinaliz
         setObservacao(obs);
     }
 
-/*
-    const onTrocarMesa = (mesa) => {
-        setMesa(mesa);
-    }
+    /*
+        const onTrocarMesa = (mesa) => {
+            setMesa(mesa);
+        }
 
-    const onTrocarMesaClick = () => {
-        setTrocarMesa(true);
-    }
-*/
+        const onTrocarMesaClick = () => {
+            setTrocarMesa(true);
+        }
+    */
 
     const onCancelarPedidoClick = () => {
 
@@ -221,12 +228,12 @@ export default function ListaItensCarrinho({suite_id, onCarrinhoVazio, onFinaliz
 
         //let url = '/api/order/cart/' + (mesa ? ('waiter/' + login.uuid + '/') : '') + (mesa ?? suite_id);
 
-        alerta({ 
+        alerta({
             title: 'Atenção!',
             message: 'Cancela o pedido?',
-            onYes: () => { 
+            onYes: () => {
                 api.delete(`${url}/todos`).then(
-                    response => { 
+                    response => {
                         setCarrinho(response.data);
                         setApartamentoAtual(null);
                         setMesaAtual(null);
@@ -249,7 +256,7 @@ export default function ListaItensCarrinho({suite_id, onCarrinhoVazio, onFinaliz
         }
 
         const NumeroSuite = ({suite}) => {
-            
+
             const apartamento = getApartamentoAtual();
             const ap = login.suite ?? apartamento.id;
 
@@ -259,26 +266,26 @@ export default function ListaItensCarrinho({suite_id, onCarrinhoVazio, onFinaliz
             );
         }
 
-        return(
+        return (
             <div className="container-product-info">
 
                 <div style={{overflow: 'hidden'}}>
                     <div className="total-carrinho">Total: {getPreco(total)}</div>
-                    <NumeroSuite suite={suite} />
+                    <NumeroSuite suite={suite}/>
                 </div>
 
-                <a className="fecha-pedido" href="#" onClick={onEnviarPedidoClick} 
-                    style={{color: 'white'}}>
+                <a className="fecha-pedido" href="#" onClick={onEnviarPedidoClick}
+                   style={{color: 'white'}}>
                     Enviar Pedido
                 </a>
 
-                <a className="cancela-pedido" href="#" onClick={onCancelarPedidoClick} 
-                    style={{color: 'white'}}>
+                <a className="cancela-pedido" href="#" onClick={onCancelarPedidoClick}
+                   style={{color: 'white'}}>
                     Cancelar Pedido
                 </a>
-                
-                <br />
-                
+
+                <br/>
+
             </div>
         )
     }
@@ -290,32 +297,32 @@ export default function ListaItensCarrinho({suite_id, onCarrinhoVazio, onFinaliz
             return '';
         }
 
-        return(
-            items.map(item => 
-                <div type="button" 
-                    onClick={() => onItemClick(item)}
-                    key={item.id} 
-                    className="products">
-                    <div style={{padding: '10px'}}>    
-                        <p style={{fontSize:14, color:'#a2a276'}}>Nº {item.codigo}</p>
-                        <p style={{color:'#3fe799', fontSize:16, maxWidth: 200}}>{item.descricao}</p>
-                        <p style={{fontSize:14, width: 300}}>{item.nota}</p>
-                        <p style={{fontSize:18}}>
-                            <span style={{color:'#8c8c8c'}}>{getPreco(item.preco, item.quantidade)}</span>
+        return (
+            items.map(item =>
+                <div type="button"
+                     onClick={() => onItemClick(item)}
+                     key={item.id}
+                     className="products">
+                    <div style={{padding: '10px'}}>
+                        <p style={{fontSize: 14, color: '#a2a276'}}>Nº {item.codigo}</p>
+                        <p style={{color: '#3fe799', fontSize: 16, maxWidth: 200}}>{item.descricao}</p>
+                        <p style={{fontSize: 14, width: 300}}>{item.nota}</p>
+                        <p style={{fontSize: 18}}>
+                            <span style={{color: '#8c8c8c'}}>{getPreco(item.preco, item.quantidade)}</span>
                         </p>
                         {
-                            item.atendido ? '' : <p className="toque" style={{fontSize:14}}>Toque para excluir</p>
+                            item.atendido ? '' : <p className="toque" style={{fontSize: 14}}>Toque para excluir</p>
                         }
                     </div>
                 </div>
-           )
+            )
         )
 
     };
 
     return (
         <div>
-            { 
+            {
                 mensagem ? (
 
                     <SemItensStyle>{mensagem}</SemItensStyle>
@@ -324,24 +331,26 @@ export default function ListaItensCarrinho({suite_id, onCarrinhoVazio, onFinaliz
 
                     <div>
 
-                        <Itens 
-                            items={carrinho.items} 
+                        <Itens
+                            items={carrinho.items}
                             onListaVazia={
-                                () => { setMensagem('Sem ítens!') }
-                            } />
+                                () => {
+                                    setMensagem('Sem ítens!')
+                                }
+                            }/>
 
-                        <LocalEntrega 
+                        <LocalEntrega
                             cardapio={cardapio}
                             locais={locais}
                             obsMensagemLocal={mensagemLocalizacao}
-                            onLocalizacao={onLocalizacao} 
-                            onObservacao={onObservacao} />
+                            onLocalizacao={onLocalizacao}
+                            onObservacao={onObservacao}/>
 
-                        <Painel total={carrinho.total} />
+                        <Painel total={carrinho.total}/>
 
                     </div>
 
-                ) 
+                )
             }
         </div>
     )
@@ -349,8 +358,8 @@ export default function ListaItensCarrinho({suite_id, onCarrinhoVazio, onFinaliz
 
 const SemItensStyle = styled.p`
     display: flex;
-    justify-content:center;
-    align-items:center;
+    justify-content: center;
+    align-items: center;
     font-size: 25px;
 `;
 
